dc729738a9407a9915ea16147a1526b42ee2f05f
Neil Chadwick <neilchadders@hotmail.com>
2023-05-19 08:41:37 +0100

    add directory component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import CategoryItem from './components/category-item/category-item.component';
-
+import Directory from './components/directory/directory.component';
 import './categories.styles.scss';
 
 const App = () => {
@@ -31,14 +30,7 @@ const App = () => {
     }
   ]
 
-
-  return (
-    <div className='categories-container'>
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
