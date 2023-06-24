e28f951904b8d6d6805f64472d416acc8e2108e9
Neil Chadwick <neilchadders@hotmail.com>
2023-06-24 20:23:29 +0100

    auth and get from firebase working

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,30 +1,7 @@
 import { createContext, useState, useEffect } from 'react';
 
-//import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
-import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
-
-import SHOP_DATA from '../shop-data.js';
-
-export const CategoriesContext = createContext({
-    products: [],
-});
-
-export const CategoriesProvider = ({ children }) => {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        addCollectionAndDocuments('collections', SHOP_DATA);
-    }, []);
-
-    const value = { products };
-    return (
-        <CategoriesContext.Provider value={value}>
-            {children}
-        </CategoriesContext.Provider>
-    );
-};
-/*
 export const CategoriesContext = createContext({
     categoriesMap: {},
 });
@@ -34,7 +11,8 @@ export const CategoriesProvider = ({ children }) => {
 
     useEffect(() => {
         const getCategoriesMap = async () => {
-            const categoryMap = await getCategoriesAndDocuments('categories');
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
             setCategoriesMap(categoryMap);
         };
 
@@ -48,4 +26,3 @@ export const CategoriesProvider = ({ children }) => {
         </CategoriesContext.Provider>
     );
 };
-*/
