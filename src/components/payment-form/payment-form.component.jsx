dd2560f867e43a9cac323a96c3c9ea4cac6f51d2
Neil Chadwick <neilchadders@hotmail.com>
2023-07-01 17:09:50 +0100

    fixing stripe bug

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -13,46 +13,27 @@ const PaymentForm = () => {
     const stripe = useStripe();
     const elements = useElements();
 
-    const [isProcessingPayment, setIsProcessingPayment] = useState(false);
+    //const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
     const paymentHandler = async (e) => {
         e.preventDefault();
         if (!stripe || !elements) {
             return;
         }
-        setIsProcessingPayment(true);
+
         const response = await fetch('/.netlify/functions/create-payment-intent', {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ amount: 100 }),
-        }).then((res) => {
-            return res.json();
-        });
+            body: JSON.stringify({ amount: 1000 }),
+        }).then((res) => res.json());
 
-        const clientSecret = response.paymentIntent.client_secret;
+        console.log(response)
 
-        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
-            payment_method: {
-                card: elements.getElement(CardElement),
-                billing_details: {
-                    name: "Home"
-                    //name: currentUser ? currentUser.displayName : 'Yihua Zhang',
-                },
-            },
-        });
-
-        setIsProcessingPayment(false);
-
-        if (paymentResult.error) {
-            alert(paymentResult.error.message);
-        } else {
-            if (paymentResult.paymentIntent.status === 'succeeded') {
-                alert('Payment Successful!');
-            }
-        }
     };
+
+
 
     return (
         <PaymentFormContainer>
@@ -61,7 +42,7 @@ const PaymentForm = () => {
                 <CardElement />
                 <PaymentButton
                     buttonType={BUTTON_TYPE_CLASSES.inverted}
-                // isLoading={isProcessingPayment}
+
                 >
                     Pay Now
                 </PaymentButton>
