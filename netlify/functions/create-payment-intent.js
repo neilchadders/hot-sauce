d5f614ae9086e51a24eaa5b1f8c25ceebf7f2cbd
Neil Chadwick <neilchadders@hotmail.com>
2023-07-01 18:05:30 +0100

    amend USD to GBP

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.REACT_APP_STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
     try {
@@ -7,7 +7,7 @@ exports.handler = async (event) => {
 
         const paymentIntent = await stripe.paymentIntents.create({
             amount,
-            currency: "usd",
+            currency: "gbp",
             payment_method_types: ["card"],
         });
 
