e81a813da169bbe3095101a24e1d5a4a056852fc
Neil Chadwick <neilchadders@hotmail.com>
2023-07-02 13:31:43 +0100

    making mobile friendly -add media queries

--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -5,6 +5,20 @@ export const CategoryContainer = styled.div`
   grid-template-columns: repeat(4, 1fr);
   column-gap: 20px;
   row-gap: 50px;
+  margin-left: 20px;
+  margin-right: 20px;
+
+
+  @media screen and (max-width: 1300px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media screen and (max-width: 1000px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media screen and (max-width: 700px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
 `;
 
 export const Title = styled.h2`
